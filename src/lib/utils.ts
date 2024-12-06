d6812d547e74b95e509673525721b9daa72d14f0
oyelsv <oyelsv@gmail.com>
2024-12-06 21:40:00 +0100

    chore(utils): add util functions for emoji flags and Intl date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getFlagEmoji(countryCode: string) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map((char) => 127397 + char.charCodeAt(0));
+
+  return String.fromCodePoint(...codePoints);
+}
+
+export function getIntlDateFormat(date: Date, lang?: string, options?: Intl.DateTimeFormatOptions): string {
+  return new Intl.DateTimeFormat(lang || 'en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    ...options,
+  }).format(date);
+}
