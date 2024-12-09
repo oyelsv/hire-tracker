18c0bc35319bda3128d471f60b43ea3b9301f4ee
oyelsv <oyelsv@gmail.com>
2024-12-09 13:45:02 +0100

    feat: add next-auth

---
+++ src/auth.ts
@@ -0,0 +1,14 @@
+import NextAuth from 'next-auth';
+
+import authConfig from '@/auth.config';
+
+export const {
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+  auth,
+} = NextAuth({
+  // @TODO: PrismaAdapter,
+  // strategy: 'jwt',
+  ...authConfig,
+});
---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import { PROJECTS_ROUTES } from '@/app/(protected)/projects/constants';
+
+export const publicRoutes = ['/about'];
+
+export const authRoutes = ['/auth/login'];
+
+export const apiAuthPrefix = '/api/auth';
+
+export const BASE_SEGMENT_REDIRECT = PROJECTS_ROUTES.ROOT;
+
+export const DEFAULT_LOGIN_REDIRECT = PROJECTS_ROUTES.ROOT;
