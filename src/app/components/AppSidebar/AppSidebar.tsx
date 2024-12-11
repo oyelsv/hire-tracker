4ec49698d32a300e16baf5e20c6f55bb08fc10d9
oyelsv <oyelsv@gmail.com>
2024-12-11 21:47:56 +0100

    feat(sidebar): add contacts

--- src/app/components/AppSidebar/AppSidebar.tsx
+++ src/app/components/AppSidebar/AppSidebar.tsx
@@ -2,7 +2,8 @@
 
 import { useTheme } from 'next-themes';
 import { signOut } from 'next-auth/react';
-import { Coffee, LogOut, Moon, Sun } from 'lucide-react';
+import { Coffee, Linkedin, Github, LogOut, Moon, Sun } from 'lucide-react';
+import Link from 'next/link';
 
 import { useCurrentUser } from '@/hooks/useCurrentUser';
 
@@ -13,8 +14,11 @@ import {
   SidebarGroup,
   SidebarGroupLabel,
   SidebarHeader,
+  SidebarSeparator,
 } from '@/components/ui/sidebar';
-import { Button } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
+
+import packageJson from '../../../../package.json';
 
 export function AppSidebar() {
   const user = useCurrentUser();
@@ -24,17 +28,20 @@ export function AppSidebar() {
     /* @TODO: make Sidebar floating */
     <Sidebar>
       <SidebarHeader>
-        <div className="flex justify-end">
-          {theme === 'light' && (
-            <Button size="icon" variant="outline" onClick={() => setTheme('dark')}>
-              <Moon />
-            </Button>
-          )}
-          {theme === 'dark' && (
-            <Button size="icon" variant="outline" onClick={() => setTheme('light')}>
-              <Sun />
-            </Button>
-          )}
+        <div className="flex w-full items-center pl-2">
+          <div>Logo</div>
+          <div className="ml-auto">
+            {theme === 'light' && (
+              <Button size="icon" variant="outline" onClick={() => setTheme('dark')}>
+                <Moon />
+              </Button>
+            )}
+            {theme === 'dark' && (
+              <Button size="icon" variant="ghost" onClick={() => setTheme('light')}>
+                <Sun />
+              </Button>
+            )}
+          </div>
         </div>
       </SidebarHeader>
       <SidebarContent>
@@ -45,6 +52,43 @@ export function AppSidebar() {
         <SidebarGroup />
       </SidebarContent>
       <SidebarFooter>
+        <div>
+          <small className="text-xs font-medium leading-none">
+            <span className="after:content-['\a0']">Developed by:</span>
+            <a href="mailto:oyelsv@gmail.com" className="underline">
+              Oleksandr Yeliseev
+            </a>
+          </small>
+          <div className="">
+            <span className="text-sm">My socials:</span>
+            <div className="flex gap-x-1 mt-1">
+              <Link
+                target="_blank"
+                href="https://github.com/oyelsv"
+                className={buttonVariants({ variant: 'secondary', size: 'icon' })}
+              >
+                <Github />
+              </Link>
+              <Link
+                target="_blank"
+                href="https://www.linkedin.com/in/oyelsv/"
+                className={buttonVariants({ variant: 'secondary', size: 'icon' })}
+              >
+                <Linkedin />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <SidebarSeparator />
+        <div>
+          <Link
+            href="https://github.com/oyelsv/hire-tracker/releases"
+            target="_blank"
+            className="text-xs text-muted-foreground"
+          >
+            Version: <span className="underline">{packageJson.version}</span>
+          </Link>
+        </div>
         <div className="flex justify-between">
           <Button variant="outline" size="icon">
             <Coffee />
