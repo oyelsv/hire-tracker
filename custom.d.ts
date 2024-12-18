0d28edae930c5b42da0cffc9ccb1a08481618222
oyelsv <oyelsv@gmail.com>
2024-12-18 12:30:35 +0100

    feat(i18n): add translations

--- custom.d.ts
+++ custom.d.ts
@@ -1,3 +1,7 @@
+import { formats } from '@/i18n/request';
+
+type Formats = typeof formats;
+
 declare module '*.svg' {
   import { FC, SVGProps } from 'react';
 
@@ -8,4 +12,8 @@ declare module '*.svg' {
 declare module '*.svg?url' {
   const content: any;
   export default content;
+}
+
+declare global {
+  interface IntlFormats extends Formats {}
 }
